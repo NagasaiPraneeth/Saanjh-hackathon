9925e4f80529a3f3d9c5ce253265c3b2b41a32fc
nagasai <123751071+NagasaiPraneeth@users.noreply.github.com>
2024-10-08 17:56:01 +0530

    hello

---
+++ Saanjh-sahayak-frontend1/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Change this to 'dist' for Vercel compatibility
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
+})
